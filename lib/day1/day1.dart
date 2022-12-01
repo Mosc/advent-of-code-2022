ac753a40d78914527765cac55e716e49d31f2cf5
Mosc <daniel.moscoviter@gmail.com>
2022-12-01 09:01:29 +0100

    Solve day 1 part 2

--- lib/day1/day1.dart
+++ lib/day1/day1.dart
@@ -1,25 +1,33 @@
 import 'dart:io';
-import 'dart:math';
 
 import 'package:collection/collection.dart';
 
 Future<void> calculate() async {
-  await _calculatePart1();
+  final lines = File('lib/day1/input').readAsLinesSync();
+
+  final caloriesPerElf =
+      lines.split((line) => line.isEmpty).map((line) => line.map(int.parse));
+  final caloriesSumsPerElf = caloriesPerElf.map((calories) => calories.sum);
+
+  var part1 = await _calculatePart1(caloriesSumsPerElf);
+  var part2 = await _calculatePart2(caloriesSumsPerElf);
+
+  print('Day 1');
+  print('Part 1: $part1');
+  print('Part 2: $part2');
+  print('');
 }
 
-Future<void> _calculatePart1() async {
-  final Iterable<String> lines = File('lib/day1/input').readAsLinesSync();
+Future<int> _calculatePart1(Iterable<int> caloriesSums) async {
+  return caloriesSums.max;
+}
 
-  final caloriesPerElf = lines
-      .split(on: (line) => line.isEmpty)
-      .map((line) => line.map(int.parse));
-  final caloriesSumPerElf = caloriesPerElf.map((calories) => calories.sum);
-  final maxCalories = caloriesSumPerElf.reduce(max);
-  print(maxCalories);
+Future<int> _calculatePart2(Iterable<int> caloriesSums) async {
+  return caloriesSums.sorted((a, b) => b.compareTo(a)).take(3).sum;
 }
 
 extension _Iterablextension<T> on Iterable<T> {
-  Iterable<Iterable<T>> split({required bool Function(T) on}) sync* {
+  Iterable<Iterable<T>> split(bool Function(T) on) sync* {
     final List<T> innerList = [];
 
     for (final row in this) {
