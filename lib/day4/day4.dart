9a10d40c390e3e4d36cdb8c51908c0b357043f41
Mosc <daniel.moscoviter@gmail.com>
2022-12-04 08:45:19 +0100

    Solve day 4 part 2

--- lib/day4/day4.dart
+++ lib/day4/day4.dart
@@ -2,15 +2,6 @@ import 'dart:io';
 
 Future<void> calculate() async {
   final lines = File('lib/day4/input').readAsLinesSync();
-
-  var part1 = await _calculatePart1(lines);
-
-  print('Day 3');
-  print('Part 1: $part1');
-  print('');
-}
-
-Future<int> _calculatePart1(Iterable<String> lines) async {
   final sections = lines.map(
     (line) => line.split(',').map((sections) {
       final split = sections.split('-').map(int.parse);
@@ -19,11 +10,31 @@ Future<int> _calculatePart1(Iterable<String> lines) async {
       return List.generate(to - from + 1, (index) => from + index).toSet();
     }),
   );
+
+  var part1 = await _calculatePart1(sections);
+  var part2 = await _calculatePart2(sections);
+
+  print('Day 3');
+  print('Part 1: $part1');
+  print('Part 2: $part2');
+  print('');
+}
+
+Future<int> _calculatePart1(Iterable<Iterable<Set<int>>> sections) async {
   return sections
       .where(
         (sectionPair) =>
             (sectionPair.first..removeAll(sectionPair.last)).isEmpty ||
             (sectionPair.last..removeAll(sectionPair.first)).isEmpty,
+      )
+      .length;
+}
+
+Future<int> _calculatePart2(Iterable<Iterable<Set<int>>> sections) async {
+  return sections
+      .where(
+        (sectionPair) =>
+            sectionPair.first.intersection(sectionPair.last).isNotEmpty,
       )
       .length;
 }
