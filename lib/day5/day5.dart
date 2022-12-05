183fffa2bc5ad6a8a7c659b38f7e12f6adac65a9
Mosc <daniel.moscoviter@gmail.com>
2022-12-05 08:15:38 +0100

    Solve day 5 part 1

---
+++ lib/day5/day5.dart
@@ -0,0 +1,48 @@
+import 'dart:collection';
+import 'dart:io';
+
+Future<void> calculate() async {
+  final lines = File('lib/day5/input').readAsLinesSync();
+
+  var part1 = await _calculatePart1(lines);
+
+  print('Day 5');
+  print('Part 1: $part1');
+  print('');
+}
+
+Future<String> _calculatePart1(List<String> lines) async {
+  final stackEndIndex = lines.indexOf('');
+  final stacks = <int, Queue<String>>{};
+
+  for (final line in lines.sublist(0, stackEndIndex - 1)) {
+    final stackCount = (line.length + 1) ~/ 4;
+
+    for (var i = 1; i < stackCount + 1; i++) {
+      if (stacks[i] == null) {
+        stacks[i] = Queue<String>();
+      }
+
+      var crate = line[i * 4 - 3];
+
+      if (crate != ' ') {
+        stacks[i]!.addFirst(crate);
+      }
+    }
+  }
+
+  for (final line in lines.sublist(stackEndIndex + 1)) {
+    final words = line.split(' ');
+    final amount = int.parse(words[1]);
+    final from = int.parse(words[3]);
+    final to = int.parse(words[5]);
+
+    for (int i = 0; i < amount; i++) {
+      final crate = stacks[from]!.last;
+      stacks[from]!.removeLast();
+      stacks[to]!.addLast(crate);
+    }
+  }
+
+  return stacks.values.map((stack) => stack.last).join();
+}
