822d2fe3f091a6fa5915efd35744c1a4a726e830
Mosc <daniel.moscoviter@gmail.com>
2022-12-10 11:48:59 +0100

    Solve day 10 part 2

--- lib/day10/day10.dart
+++ lib/day10/day10.dart
@@ -1,16 +1,16 @@
 import 'package:advent_of_code_2022/day.dart';
 import 'package:collection/collection.dart';
 
-class Day10 extends Day<List<String>, int> {
+class Day10 extends Day<List<String>, String> {
   const Day10() : super(10);
 
   @override
-  int processPart1(List<String> value) {
-    var cycle = 1;
-    var x = 1;
+  String processPart1(List<String> value) {
     final interestingCycles =
         List<int>.generate(6, (index) => index * 40 + 20, growable: false);
     final signalStrengths = <int, int>{};
+    var cycle = 1;
+    var x = 1;
 
     for (final line in value) {
       final tokens = line.split(' ');
@@ -35,7 +35,46 @@ class Day10 extends Day<List<String>, int> {
           (signalStrength) => interestingCycles.contains(signalStrength.key),
         )
         .map((signalStrength) => signalStrength.value)
-        .sum;
+        .sum
+        .toString();
+  }
+
+  @override
+  String processPart2(List<String> value) {
+    const rowLength = 40;
+    final crt = <List<bool>>[];
+    var cycle = 1;
+    var x = 1;
+
+    for (final line in value) {
+      final tokens = line.split(' ');
+      late final Op op;
+
+      if (tokens[0] == 'addx') {
+        op = Op(cycleLength: 2, execute: () => x += int.parse(tokens[1]));
+      } else if (tokens[0] == 'noop') {
+        op = Op(cycleLength: 1);
+      }
+
+      for (var i = 0; i < op.cycleLength; i++) {
+        final row = (cycle - 1) ~/ rowLength;
+
+        if (row > crt.length - 1) {
+          crt.add(<bool>[]);
+        }
+
+        crt[row].add((cycle % rowLength - x - 1).abs() <= 1);
+
+        cycle++;
+      }
+
+      op.execute?.call();
+    }
+
+    return [
+      '',
+      crt.map((row) => row.map((pixel) => pixel ? '#' : '.').join()).join('\n')
+    ].join('\n');
   }
 }
 
