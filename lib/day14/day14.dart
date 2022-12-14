ef450dabe1f538d8638276daec215c0115043f8d
Mosc <daniel.moscoviter@gmail.com>
2022-12-14 21:14:51 +0100

    Solve day 14 part 2

--- lib/day14/day14.dart
+++ lib/day14/day14.dart
@@ -43,14 +43,15 @@ class Day14 extends Day<List<String>, int> {
       }
     }
 
+    final maxY = map.keys.max;
     var sandCount = 0;
     var fallingIntoAbyss = false;
 
-    while (!fallingIntoAbyss) {
+    while (true) {
       var sandPoint = startingPoint;
 
       while (true) {
-        if (sandPoint.y >= map.keys.max) {
+        if (sandPoint.y >= maxY) {
           fallingIntoAbyss = true;
           break;
         } else if (map[sandPoint.y + 1]?[sandPoint.x] == null) {
@@ -69,8 +70,87 @@ class Day14 extends Day<List<String>, int> {
         }
       }
 
-      if (!fallingIntoAbyss) {
-        sandCount++;
+      if (fallingIntoAbyss) {
+        break;
+      }
+
+      sandCount++;
+    }
+
+    return sandCount;
+  }
+
+  @override
+  int processPart2(List<String> input) {
+    const startingPoint = Point(500, 0);
+
+    final map = <int, Map<int, Element>>{};
+
+    for (final line in input) {
+      final rockLines = line
+          .split(' -> ')
+          .map((point) => point.split(',').map(int.parse))
+          .map((coordinates) => Point(coordinates.first, coordinates.last))
+          .toList();
+
+      for (int i = 0; i < rockLines.length - 1; i++) {
+        final from = rockLines[i];
+        final to = rockLines[i + 1];
+
+        if (from.x == to.x) {
+          for (int j = min(from.y, to.y); j <= max(from.y, to.y); j++) {
+            if (map[j] == null) {
+              map[j] = <int, Element>{};
+            }
+
+            map[j]![from.x] = Element.rock;
+          }
+        } else {
+          for (int j = min(from.x, to.x); j <= max(from.x, to.x); j++) {
+            if (map[from.y] == null) {
+              map[from.y] = <int, Element>{};
+            }
+
+            map[from.y]![j] = Element.rock;
+          }
+        }
+      }
+    }
+
+    final maxY = map.keys.max + 1;
+    var sandCount = 0;
+
+    while (true) {
+      var sandPoint = startingPoint;
+
+      while (true) {
+        if (sandPoint.y >= maxY) {
+          if (map[sandPoint.y] == null) {
+            map[sandPoint.y] = <int, Element>{};
+          }
+
+          map[sandPoint.y]![sandPoint.x] = Element.sand;
+          break;
+        } else if (map[sandPoint.y + 1]?[sandPoint.x] == null) {
+          sandPoint += Point(0, 1);
+        } else if (map[sandPoint.y + 1]?[sandPoint.x - 1] == null) {
+          sandPoint += Point(-1, 1);
+        } else if (map[sandPoint.y + 1]?[sandPoint.x + 1] == null) {
+          sandPoint += Point(1, 1);
+        } else {
+          if (map[sandPoint.y] == null) {
+            map[sandPoint.y] = <int, Element>{};
+          }
+
+          map[sandPoint.y]![sandPoint.x] = Element.sand;
+          break;
+        }
+      }
+
+      sandCount++;
+
+      if (sandPoint == startingPoint) {
+        break;
       }
     }
 
