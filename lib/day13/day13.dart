da602372ea5b03af5d913c7550c1e82c87d271b8
Mosc <daniel.moscoviter@gmail.com>
2022-12-14 23:06:48 +0100

    Solve day 13 part 2

--- lib/day13/day13.dart
+++ lib/day13/day13.dart
@@ -3,51 +3,74 @@ import 'dart:convert';
 import 'package:advent_of_code_2022/day.dart';
 import 'package:collection/collection.dart';
 
-class Day13 extends Day<Iterable<bool>, int> {
+class Day13 extends Day<Iterable<dynamic>, int> {
   const Day13() : super(13);
 
   @override
-  Iterable<bool> preprocess(List<String> input) sync* {
+  Iterable<dynamic> preprocess(List<String> input) sync* {
     for (int i = 0; i < input.length; i += 3) {
-      final left = jsonDecode(input[i]) as List<dynamic>;
-      final right = jsonDecode(input[i + 1]) as List<dynamic>;
-      yield _isInOrder(left, right)!;
+      yield jsonDecode(input[i]) as List<dynamic>;
+      yield jsonDecode(input[i + 1]) as List<dynamic>;
     }
   }
 
   @override
-  int processPart1(Iterable<bool> input) =>
-      input.mapIndexed((index, inOrder) => inOrder ? index + 1 : 0).sum;
+  int processPart1(Iterable<dynamic> input) {
+    final inOrders = _getInOrders([...input]);
+    return inOrders.mapIndexed((index, inOrder) => inOrder ? index + 1 : 0).sum;
+  }
 
-  bool? _isInOrder(List<dynamic> left, List<dynamic> right) {
+  @override
+  int processPart2(Iterable<dynamic> input) {
+    const dividers = [
+      [
+        [2]
+      ],
+      [
+        [6]
+      ],
+    ];
+    final lines = [...input, ...dividers].sorted((a, b) => _isInOrder(a, b));
+    return dividers
+        .map((divider) => lines.indexOf(divider) + 1)
+        .reduce((total, index) => total * index);
+  }
+
+  Iterable<bool> _getInOrders(List<dynamic> lines) sync* {
+    for (int i = 0; i < lines.length; i += 2) {
+      yield _isInOrder(lines[i], lines[i + 1]) < 0;
+    }
+  }
+
+  int _isInOrder(List<dynamic> left, List<dynamic> right) {
     for (int i = 0; i < left.length; i++) {
-      final bool? inOrder;
+      final int result;
 
       if (i >= right.length) {
-        inOrder = false;
+        result = 1;
       } else if (left[i] is List<dynamic> && right[i] is List<dynamic>) {
-        inOrder = _isInOrder(left[i], right[i]);
+        result = _isInOrder(left[i], right[i]);
       } else if (left[i] is int && right[i] is List<dynamic>) {
-        inOrder = _isInOrder([left[i]], right[i]);
+        result = _isInOrder([left[i]], right[i]);
       } else if (left[i] is List<dynamic> && right[i] is int) {
-        inOrder = _isInOrder(left[i], [right[i]]);
+        result = _isInOrder(left[i], [right[i]]);
       } else if (left[i] < right[i]) {
-        inOrder = true;
+        result = -1;
       } else if (left[i] > right[i]) {
-        inOrder = false;
+        result = 1;
       } else {
-        inOrder = null;
+        result = 0;
       }
 
-      if (inOrder != null) {
-        return inOrder;
+      if (result != 0) {
+        return result;
       }
     }
 
     if (left.length < right.length) {
-      return true;
+      return -1;
     }
 
-    return null;
+    return 0;
   }
 }
