64a9292733845a7d89b65b93ef66d439addee81b
Mosc <daniel.moscoviter@gmail.com>
2022-12-15 19:40:23 +0100

    Solve day 15 part 2

--- lib/day15/day15.dart
+++ lib/day15/day15.dart
@@ -3,13 +3,12 @@ import 'dart:math';
 import 'package:advent_of_code_2022/day.dart';
 import 'package:collection/collection.dart';
 
-class Day15 extends Day<List<String>, int> {
+class Day15 extends Day<Map<Point<int>, Point<int>>, int> {
   const Day15() : super(15);
 
   @override
-  int processPart1(List<String> input) {
-    const y = 2000000;
-    final sensors = <Point<int>, Point<int>>{};
+  Map<Point<int>, Point<int>> preprocess(List<String> input) {
+    final beacons = <Point<int>, Point<int>>{};
 
     for (final line in input) {
       final xMatches = RegExp(r'x=(-?\d+)')
@@ -22,22 +21,27 @@ class Day15 extends Day<List<String>, int> {
           .map(int.parse);
       final sensor = Point(xMatches.first, yMatches.first);
       final beacon = Point(xMatches.last, yMatches.last);
-      sensors[sensor] = beacon;
+      beacons[sensor] = beacon;
     }
 
-    final minX = sensors.entries
-        .map(
-          (entry) => entry.key.x - entry.key.manhattanDistanceTo(entry.value),
-        )
-        .min;
-    final maxX = sensors.entries
-        .map(
-          (entry) => entry.key.x + entry.key.manhattanDistanceTo(entry.value),
-        )
-        .max;
+    return beacons;
+  }
+
+  @override
+  int processPart1(Map<Point<int>, Point<int>> input) {
+    final beacons = input;
+    final distances = beacons.map(
+      (key, value) => MapEntry(key, key.manhattanDistanceTo(value)),
+    );
+    final minX =
+        distances.entries.map((entry) => entry.key.x - entry.value).min;
+    final maxX =
+        distances.entries.map((entry) => entry.key.x + entry.value).max;
+    final y = example ? 10 : 2000000;
+
     return Iterable.generate(maxX - minX + 1, (index) => minX + index)
         .where(
-          (x) => sensors.entries.any(
+          (x) => beacons.entries.any(
             (entry) =>
                 entry.key.manhattanDistanceTo(Point(x, y)) <=
                     entry.key.manhattanDistanceTo(entry.value) &&
@@ -45,6 +49,34 @@ class Day15 extends Day<List<String>, int> {
           ),
         )
         .length;
+  }
+
+  @override
+  int processPart2(Map<Point<int>, Point<int>> input) {
+    final distances = input.map(
+      (key, value) => MapEntry(key, key.manhattanDistanceTo(value)),
+    );
+
+    const minXY = 0;
+    final maxXY = example ? 20 : 4000000;
+    const xMultiplier = 4000000;
+
+    for (int y = minXY; y <= maxXY; y++) {
+      for (int x = minXY; x <= maxXY; x++) {
+        final point = Point(x, y);
+        final overlap = distances.entries.firstWhereOrNull(
+          (entry) => entry.key.manhattanDistanceTo(point) <= entry.value,
+        );
+
+        if (overlap == null) {
+          return x * xMultiplier + y;
+        }
+
+        x = overlap.key.x + overlap.value - (overlap.key.y - y).abs();
+      }
+    }
+
+    throw Exception();
   }
 }
 
