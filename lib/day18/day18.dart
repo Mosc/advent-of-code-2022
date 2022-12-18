04814db658fefff1f5f0ad2b7c0f5d6561fb975f
Mosc <daniel.moscoviter@gmail.com>
2022-12-18 12:12:35 +0100

    Solve day 18 part 2

--- lib/day18/day18.dart
+++ lib/day18/day18.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:advent_of_code_2022/day.dart';
 import 'package:collection/collection.dart';
 
@@ -17,6 +19,43 @@ class Day18 extends Day<Set<Cube>, int> {
             cube.adjacent.where((adjacent) => !input.contains(adjacent)).length,
       )
       .sum;
+
+  @override
+  int processPart2(Set<Cube> input) {
+    final minX = input.map((cube) => cube.x).min - 1;
+    final maxX = input.map((cube) => cube.x).max + 1;
+    final minY = input.map((cube) => cube.y).min - 1;
+    final maxY = input.map((cube) => cube.y).max + 1;
+    final minZ = input.map((cube) => cube.z).min - 1;
+    final maxZ = input.map((cube) => cube.z).max + 1;
+
+    bool isWithinBounds(Cube cube) =>
+        cube.x >= minX &&
+        cube.x <= maxX &&
+        cube.y >= minY &&
+        cube.y <= maxY &&
+        cube.z >= minZ &&
+        cube.z <= maxZ;
+
+    final queue = Queue<Cube>()..add(Cube(minX, minY, minZ));
+    final water = <Cube>{};
+    var surfaceArea = 0;
+
+    while (queue.isNotEmpty) {
+      final cube = queue.removeFirst();
+
+      for (final adjacent in cube.adjacent) {
+        if (input.contains(adjacent)) {
+          surfaceArea++;
+        } else if (!water.contains(adjacent) && isWithinBounds(adjacent)) {
+          queue.add(adjacent);
+          water.add(adjacent);
+        }
+      }
+    }
+
+    return surfaceArea;
+  }
 }
 
 class Cube {
