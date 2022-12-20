0d50db0f0eeaae1048c0c512cfbba8da51b43e02
Mosc <daniel.moscoviter@gmail.com>
2022-12-20 12:14:55 +0100

    Solve day 20 part 2

--- lib/day20/day20.dart
+++ lib/day20/day20.dart
@@ -1,26 +1,49 @@
 import 'package:advent_of_code_2022/day.dart';
 import 'package:collection/collection.dart';
 
-class Day20 extends Day<List<String>, int> {
+class Day20 extends Day<List<MapEntry<int, int>>, List<MapEntry<int, int>>> {
   const Day20() : super(20);
 
   @override
-  int processPart1(List<String> input) {
-    final mixed = input
-        .map(int.parse)
-        .mapIndexed((index, value) => MapEntry(index, value))
+  List<MapEntry<int, int>> preprocess(List<String> input) => input
+      .map(int.parse)
+      .mapIndexed((index, value) => MapEntry(index, value))
+      .toList();
+
+  @override
+  List<MapEntry<int, int>> processPart1(List<MapEntry<int, int>> input) =>
+      _mix(input);
+
+  @override
+  List<MapEntry<int, int>> processPart2(List<MapEntry<int, int>> input) {
+    const decryptionKey = 811589153;
+    final decrypted = input
+        .map((indexed) => MapEntry(indexed.key, indexed.value * decryptionKey))
         .toList();
 
-    for (var i = 0; i < mixed.length; i++) {
-      final from = mixed.indexWhere((indexed) => indexed.key == i);
-      final to = (from + mixed[from].value) % (mixed.length - 1);
-      mixed.move(from, to);
+    for (var i = 0; i < 10; i++) {
+      _mix(decrypted);
     }
 
-    final zeroIndex = mixed.indexWhere((indexed) => indexed.value == 0);
+    return decrypted;
+  }
+
+  List<MapEntry<int, int>> _mix(List<MapEntry<int, int>> input) {
+    for (var i = 0; i < input.length; i++) {
+      final from = input.indexWhere((indexed) => indexed.key == i);
+      final to = (from + input[from].value) % (input.length - 1);
+      input.move(from, to);
+    }
+
+    return input;
+  }
+
+  @override
+  int postprocess(List<MapEntry<int, int>> input) {
+    final zeroIndex = input.indexWhere((indexed) => indexed.value == 0);
     return [1000, 2000, 3000]
-        .map((index) => (zeroIndex + index) % mixed.length)
-        .map((index) => mixed[index].value)
+        .map((index) => (zeroIndex + index) % input.length)
+        .map((index) => input[index].value)
         .sum;
   }
 }
